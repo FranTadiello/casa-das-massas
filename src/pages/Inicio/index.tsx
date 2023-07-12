b8e7388f8935549613c8adc7e0b474f6b0082d9f
Franciele Tadiello <franciele.p.t@gmail.com>
2023-07-12 14:00:27 -0300

    Reaproveitar Menu

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,12 +1,14 @@
 import cardapio from 'data/cardapio.json';
 import styles from './inicio.module.scss';
+import Menu from 'components/Menu';
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
   pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0,3);
   return (
     <section>
-      <h3>
+      <Menu />
+      <h3 className={styles.titulo}>
               Recomendações da cozinha
       </h3>
       <div className={styles.recomendados}>
